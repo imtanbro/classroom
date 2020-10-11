af5971442948c1184c180dd78f0d1107b91ad64c
Tanmay Gangurde <48478512+imtanbro@users.noreply.github.com>
2020-10-11 21:05:21 +0530

    Added Firebase Auth

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,25 +1,61 @@
+// import 'package:classroom/models/user.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+//
+// class AuthService {
+//   FirebaseAuth _auth = FirebaseAuth.instance;
+//   User _userFromFirebaseUser(FirebaseUser user) async => user != null ? User(uid: user.uid) : null;
+//
+//   Future signInEmailAndPass(String email, String password) async {
+//     try {
+//       // AuthResult authResult = await _auth.signInWithEmailAndPassword(
+//       //     email: email, password: password);
+//       // FirebaseUser firebaseUser = authResult.user;
+//
+//       FirebaseUser user = (await FirebaseAuth.instance
+//               .signInWithEmailAndPassword(email: email, password: password))
+//           .user;
+//       return _userFromFirebaseUser(firebaseUser);
+//
+//     } catch (e) {
+//       print(e.toString());
+//     }
+//   }
+// }
 import 'package:classroom/models/user.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-class AuthService {
+class AuthService{
   FirebaseAuth _auth = FirebaseAuth.instance;
-  User _userFromFirebaseUser(FirebaseUser user) {
+
+  User _userFormFirebaseUser(FirebaseUser user){
     return user != null ? User(uid: user.uid) : null;
   }
 
   Future signInEmailAndPass(String email, String password) async {
-    try {
-      // AuthResult authResult = await _auth.signInWithEmailAndPassword(
-      //     email: email, password: password);
-      // FirebaseUser firebaseUser = authResult.user;
-
-      FirebaseUser user = (await FirebaseAuth.instance
-              .signInWithEmailAndPassword(email: email, password: password))
-          .user;
-      return _userFromFirebaseUser(firebaseUser);
-
-    } catch (e) {
+    try{
+      AuthResult authResult = await _auth.signInWithEmailAndPassword(email: email, password: password);
+      FirebaseUser firebaseUser = authResult.user;
+      return _userFormFirebaseUser(firebaseUser);
+    }catch(e){
+      print(e.toString())
+    }
+  }
+  Future signUpWithEmailAndPassword(String email, String password) async{
+    try{
+      AuthResult authResult = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+      FirebaseUser firebaseUser = authResult.user;
+      return _userFormFirebaseUser(firebaseUser);
+    }catch(e){
       print(e.toString());
+    }
+  }
+
+  Future signOut() async{
+    try{
+      return await _auth.signOut();
+    }catch(e){
+      print(e.toString());
+      return null;
     }
   }
 }
