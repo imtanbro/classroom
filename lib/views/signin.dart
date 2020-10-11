eca891cebfba62c666dbc5fe24799bafbaa2f3fe
Tanmay Gangurde <48478512+imtanbro@users.noreply.github.com>
2020-10-11 13:19:22 +0530

    Creating Custom UI

--- lib/views/signin.dart
+++ lib/views/signin.dart
@@ -1,3 +1,4 @@
+import 'package:classroom/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
 class SignIn extends StatefulWidget {
@@ -9,7 +10,12 @@ class _SignInState extends State<SignIn> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: appBar(context),
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        brightness: Brightness.light,
+      ),
     );
   }
 }
--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,3 +1,25 @@
 import 'package:flutter/material.dart';
 
-w
+Widget appBar(BuildContext context) {
+  
+  return Center(
+    child: RichText(
+      text: TextSpan(
+        style: TextStyle(
+          fontSize: 30,
+          ),
+        children: <TextSpan>[
+          TextSpan(text: 'G', style: TextStyle(fontWeight: FontWeight.w500, color: Color.fromRGBO(66, 133, 244, 1))),
+          TextSpan(text: 'o', style: TextStyle(fontWeight: FontWeight.w500, color: Color.fromRGBO(219, 68, 55, 1))),
+          TextSpan(text: 'o', style: TextStyle(fontWeight: FontWeight.w500, color: Color.fromRGBO(244, 180, 0, 1))),
+          TextSpan(text: 'g', style: TextStyle(fontWeight: FontWeight.w500, color: Color.fromRGBO(66, 133, 244,1))),
+          TextSpan(text: 'l', style: TextStyle(fontWeight: FontWeight.w500, color: Color.fromRGBO(15, 157, 88, 1))),
+          TextSpan(text: 'e', style: TextStyle(fontWeight: FontWeight.w500, color: Color.fromRGBO(219, 68, 55, 1))),
+          TextSpan(text: 'K', style: TextStyle(fontWeight: FontWeight.bold, color: Color.fromRGBO(244, 180, 0, 1))),
+          TextSpan(text: 'a', style: TextStyle(fontWeight: FontWeight.bold, color: Color.fromRGBO(66, 133, 244, 1))),
+          TextSpan(text: 'r', style: TextStyle(fontWeight: FontWeight.bold, color: Color.fromRGBO(219, 68, 55, 1))),
+        ],
+      ),
+    ),
+  );
+}
