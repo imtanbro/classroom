6415de48190e3796412b39311c89697654a37ee3
Tanmay Gangurde <48478512+imtanbro@users.noreply.github.com>
2020-10-11 15:43:47 +0530

    UI

--- lib/views/signin.dart
+++ lib/views/signin.dart
@@ -20,7 +20,9 @@ class _SignInState extends State<SignIn> {
         brightness: Brightness.light,
       ),
       body: Form(
+        key: _formKey,
         child: Container(
+          margin: EdgeInsets.symmetric(horizontal: 24, vertical: 20),
           child: Column(
             children: [
               Spacer(),
@@ -34,7 +36,53 @@ class _SignInState extends State<SignIn> {
                 onChanged: (val) {
                   email = val;
                 },
-              )
+              ),
+              SizedBox(height: 10,),
+              TextFormField(
+                validator: (val) {
+                  return val.isEmpty ? "Enter Password" : null;
+                },
+                decoration: InputDecoration(
+                  hintText: "Password",
+                ),
+                onChanged: (val) {
+                  password = val;
+                },
+              ),
+              SizedBox(height: 30,),
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 10),
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors:[Color.fromRGBO(66, 133, 244, 1),
+                    Color.fromRGBO(219, 68, 55, 1),
+                    Color.fromRGBO(244, 180, 0, 1),
+                    Color.fromRGBO(15, 157, 88, 1),
+                    ],
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
+                  ),
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                height: 50,
+                alignment: Alignment.center,
+                width: MediaQuery.of(context).size.width - 48,
+                child: Text("Sign In",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  ),
+                ),
+              ),
+              SizedBox(height: 20,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Dont have an Account?", style: TextStyle(fontSize: 15),),
+                  Text(" Sign Up", style: TextStyle(fontSize: 15, decoration: TextDecoration.underline),)
+                ],
+              ),
+              SizedBox(height: 100,)
             ],
           ),
         ),
