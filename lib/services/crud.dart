6cfa2fcd8fbb69567b1fab8d62e7e0291c56b496
Tanmay Gangurde <48478512+imtanbro@users.noreply.github.com>
2020-10-18 23:26:51 +0530

    Statefull

---
+++ lib/services/crud.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CRUD extends StatefulWidget {
+  @override
+  _CRUDState createState() => _CRUDState();
+}
+
+class _CRUDState extends State<CRUD> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
